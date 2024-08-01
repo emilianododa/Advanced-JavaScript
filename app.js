ebbcdde485c7beb8a2ba519d2f7b2935dc4ee9e2
Emiliano Doda <emilianododa.webdev@gmail.com>
2024-08-01 13:23:00 +0300

    Creating a new HTML element via JavaScript

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 let anchorElement = document.querySelector("p a")
 anchorElement.href = "https://google.com"
+
+// Inserting HTML element via JavaScript
+
+let newElement = document.createElement("a")
+newElement.href = "https://pinterest.com"
+newElement.textContent = "pinterest"
+
+let firstParagraph = document.querySelector("p")
+
+firstParagraph.append(newElement)
